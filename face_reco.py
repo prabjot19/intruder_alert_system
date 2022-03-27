4d153fbdaffeab64d950c12271818cb4bc054e2e
prabjot19 <prabjotram@gmail.com>
2022-03-27 20:49:54 +0100

    several changes

--- face_reco.py
+++ face_reco.py
@@ -2,21 +2,38 @@ import numpy as np
 import face_recognition as fr
 import cv2
 import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
+# Retrieve all files from current directory
+cwd = os.getcwd()
+images = [os.path.join(cwd, f) for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
+
+# Store all the images and corresponding names into list
+known_face_encondings = []
+known_face_names = []
+for image in images:
+    list = np.array(image.split("/"))
+    name = "" + list[list.size - 1]
+    if name.__contains__(".jpeg") or name.__contains__(".png"):
+        known_face_encondings.append(fr.face_encodings(fr.load_image_file(name))[0])
+        name = name.split(".")[0]
+        known_face_names.append(name)
+
+# Start the video
 video_capture = cv2.VideoCapture(0)
-
-paolo_image = fr.load_image_file("paolo.jpeg")
-paula_image = fr.load_image_file("paula.png")
-ishmail_image = fr.load_image_file("ishmail.jpeg")
-paolo_face_encoding = fr.face_encodings(paolo_image)[0]
-paula_face_encoding = fr.face_encodings(paula_image)[0]
-ishmail_face_encoding = fr.face_encodings(ishmail_image)[0]
-
-known_face_encondings = [paolo_face_encoding,ishmail_face_encoding,paula_face_encoding]
-known_face_names = ["Paolo","Ishmail","Paula"]
-
 current_name = "Unknown"
 
+# Prepare Server
+smtp_server_name = 'smtp.gmail.com'
+server = smtplib.SMTP_SSL(smtp_server_name, 465)
+server.login("prabjotram@gmail.com", "vwujsypnlvqwovdb")
+FROM = 'prabjotram@gmail.com'
+TO = "prabjotram@gmail.com"
+
+# Detect the face
 while True:
     ret, frame = video_capture.read()
 
@@ -33,32 +50,29 @@ while True:
 
         best_match_index = np.argmin(face_distances)
 
-
-        # SERVER = "Preparation"
-        FROM = 'prabjotram@gmail.com'
-        TO = "prabjotram@gmail.com"
-
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-            print(name)
-
-            # Prepare actual message
-            message = "Subject: Home Security System\n\n %s walked in!" % name
-
-        else:
-            # Prepare actual message
-            message = "Subject: Home Security System\n\nIntruder Alert!"
-            name = "Unknown"
-
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
+        message = MIMEMultipart()
+        message['Subject'] = 'Home Security System'
+        message['From'] = FROM
+        message['To'] = TO
+
+        # Face detected
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+            # Prepare actual message
+            message.attach(MIMEText("%s walked in!" % name))
+        else:
+            # Prepare actual message
+            name = "Unknown"
+            message.attach(MIMEText("Intruder Alert!"))
+
         # Send the mail
-        smtp_server_name = 'smtp.gmail.com'
-        server = smtplib.SMTP_SSL(smtp_server_name, 465)
-        server.login("prabjotram@gmail.com","vwujsypnlvqwovdb")
+        print(current_name)
+        print(name)
         if current_name != name:
             current_name = name
-            server.sendmail(FROM, TO, message)
+            server.sendmail(FROM, TO, message.as_string())
 
     cv2.imshow('Webcam_facerecognition', frame)
 
