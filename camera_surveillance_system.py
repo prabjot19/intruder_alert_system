75fb9539899f12bebf401972e4e237f9e7d65639
prabjot19 <prabjotram@gmail.com>
2022-04-01 13:44:09 +0100

    Update camera_surveillance_system.py

--- camera_surveillance_system.py
+++ camera_surveillance_system.py
@@ -10,9 +10,13 @@ import numpy as np
 import face_recognition as fr
 import cv2
 import smtplib
+import os
 from picamera.array import PiRGBArray
 from threading import Condition
 from http import server
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
 PAGE="""\
 <html>
@@ -85,13 +89,33 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
 
 class FacialRecognition(object):
     def __init__(self):
-        prabjot_image = fr.load_image_file("prabjot.jpeg")
-        prabjot_face_encoding = fr.face_encodings(prabjot_image)[0]
+        
+        # Retrieve all files from current directory
+        cwd = os.getcwd()
+        images = [os.path.join(cwd, f) for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
 
-        self.known_face_encondings = [prabjot_face_encoding]
-        self.known_face_names = ["Prabjot"]
+        # Store all the images and corresponding names into list
+        self.known_face_encondings = []
+        self.known_face_names = []
+        for image in images:
+            list = np.array(image.split("/"))
+            name = "" + list[list.size - 1]
+            if name.__contains__(".jpeg") or name.__contains__(".png"):
+                self.known_face_encondings.append(fr.face_encodings(fr.load_image_file(name))[0])
+                name = name.split(".")[0]
+                self.known_face_names.append(name)
 
         self.current_name = "Unknown"
+        
+        # Prepare Server
+        smtp_server_name = 'smtp.gmail.com'
+        self.server = smtplib.SMTP_SSL(smtp_server_name, 465)
+        server.login("prabjotram@gmail.com", "vwujsypnlvqwovdb")
+        self.FROM = 'prabjotram@gmail.com'
+        self.TO = "prabjotram@gmail.com"
+
+        # Counter for people detected
+        count = 0;
         
     def detect_face(self,frame):
     
@@ -105,8 +129,40 @@ class FacialRecognition(object):
             face_distances = fr.face_distance(self.known_face_encondings, face_encoding)
             best_match_index = np.argmin(face_distances)
         
+            # Prepare email
+            message = MIMEMultipart()
+            message['Subject'] = 'Home Security System'
+            message['From'] = self.FROM
+            message['To'] = self.TO
+            
             if matches[best_match_index]:
-                print(self.known_face_names[best_match_index])
+                name = known_face_names[best_match_index]
+                print(name)
+                
+                # Prepare actual message
+                path = os.path.join(cwd + "/known_people", '%d.png') % count
+                cv2.imwrite(path, frame)
+                count += 1
+                message.attach(MIMEText("%s walked in!" % name))
+                message.attach(MIMEImage(open(path, 'rb').read(), name=os.path.basename(path)))
+            else:
+                name = "Unknown"
+                
+                # Prepare actual message
+                path = os.path.join(cwd + "/intruders", '%d.png') % count
+                cv2.imwrite(path, frame)
+                count += 1
+                message.attach(MIMEText("Intruder Alert!"))
+                message.attach(MIMEImage(open(path, 'rb').read(), name=os.path.basename(path)))
+                
+            # Send the mail
+            if current_name != name:
+                current_name = name
+                self.server.sendmail(FROM, TO, message.as_string())
+                
+    def stop_detecting(self):
+        server.quit()
+        cv2.destroyAllWindows()
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
@@ -116,10 +172,11 @@ with picamera.PiCamera(resolution='640x480', framerate=28) as camera:
     output = StreamingOutput()
     camera.start_recording(output, format='mjpeg')
     
+    detectFace = FacialRecognition()
     rawCapture= PiRGBArray(camera, size=(640,480))
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         image =np.array(frame.array)
-        detectFace = FacialRecognition()
+        
         detectFace.detect_face(image)
         
         rawCapture.truncate(0)
@@ -129,3 +186,4 @@ with picamera.PiCamera(resolution='640x480', framerate=28) as camera:
         server.serve_forever()
     finally:
         camera.stop_recording()
+        detectFace
